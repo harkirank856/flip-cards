eb3dc03b89a7e9d5c999218fd20c2ac801db39de
harkiran <harkiran856@gmail.com>
2022-10-19 21:12:05 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,372 @@
+<template>
+ <div class="container">
+   <center class="mt-4 mb-4">
+      <h2>Vue 3 Flip Card Working Demo</h2>
+   </center>
+   <section class="section-tours">
+      <div class="row">
+         <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--1">
+                           <img :src="loadNewDog">                                
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-1">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$129</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>300 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 1 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+          <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--1">
+                           <img :src="loadNewDog">                               
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-1">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$129</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>300 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 1 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+          <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--1">
+                           <img :src="loadNewDog">                             
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-1">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$129</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>300 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 1 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+          <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--1">
+                           <img :src="loadNewDog">                               
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-1">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$129</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>300 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 1 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+          <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--1">
+                          <img :src="loadNewDog">                                
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-1">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$129</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>300 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 1 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+          <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--1">
+                           <img :src="loadNewDog">                               
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-1">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$129</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>300 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 1 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+          <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--1">
+                          <img :src="loadNewDog">                                 
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-1">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$129</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>300 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 1 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--2">
+                          <img :src="loadNewDog">               
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-2">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$149</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>400 Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Updates for 2 year</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="my-flip-container">
+               <div class="my-flip-inner my-flip-right">
+                  <div class="my-flip-inner-wrapper">
+                     <div class="my-flip-side my-flip-front">
+                        <div class="my-flip-image my-flip-image--3">
+                        <img :src="loadNewDog">
+                        
+                       
+                        </div>
+                        <div class="my-flip-details">
+                           <h4 class="my-flip-heading">My Flip Title</h4>
+                           <div class="my-flip-text">
+                              <p>Lorem ipsum dolor sit amet adipisicing elit sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="my-flip-side my-flip-back my-flip-back-3">
+                        <div class="my-flip-back-inner">
+                           <div class="my-flip-price">
+                              <h3>$169</h3>
+                           </div>
+                           <div class="my-flip-back-text">
+                              <ul>
+                                 <li>All Widgets</li>
+                                 <li>Unlimited Section</li>
+                                 <li>500+ Line Icon</li>
+                                 <li>Support 24x7</li>
+                                 <li>Lifetime Update</li>
+                              </ul>
+                           </div>
+                           <div class="my-flip-btn-box"><a href="#0" class="my-flip-btn">Book Now</a></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+</div>
+</template>
+
+<script>
+//Bootstrap
+import './App.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+export default {
+  data() {
+    return {
+      currentDogLink:
+        "https://images.dog.ceo/breeds/chihuahua/n02085620_3407.jpg"
+    };
+  },
+  methods: {
+ loadNewDog() {
+   axios
+    .get("https://dog.ceo/api/breeds/image/random")
+    .then(response => {
+      this.currentDogLink = response.data.message;
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+ }
+},
+created() {
+  this.loadNewDog();
+}
+};
+</script>
+   
+        
+    
+
